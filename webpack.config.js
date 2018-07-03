aabc752f9870f1908941922d341f7677981cabec
Ivan Khludov <ivan.khludov.dev@gmail.com>
2018-07-03 16:47:49 +0300

    Changes in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
-        libraryTarget: 'umd'
+        libraryTarget: 'amd'
     },
     mode: 'production',
     node: {
